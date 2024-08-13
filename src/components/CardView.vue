0833ec08fd3dac3473497c8d79b0d67ebfd00110
Leonardo Souza <leogsouza@gmail.com>
2024-08-13 13:15:42 -0300

    Create comic components

---
+++ src/components/CardView.vue
@@ -0,0 +1,18 @@
+<template>
+  <article class="p-4 bg-white rounded-lg shadow-xl place-content-center text-slate-800">
+    <header>
+      <h1 class="pb-5 text-lg">
+        <slot name="header"></slot>
+      </h1>
+    </header>
+    <slot></slot>
+  </article>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/ComicsOverview.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <LoadingIndicator v-if="isLoading" text="Loading comics..." />
+    <div v-if="data && !isLoading">
+      <div class="grid grid-flow-row grid-cols-1 gap-4 md:grid-cols-2 lg:grid-">
+        <div :key="comic.id" v-for="comic in data">
+          {{  comic.title }}
+        </div>
+      </div>
+    </div>
+
+  </div>
+</template>
+
+<script setup lang="ts">
+import {ref, onMounted} from 'vue'
+import type {Ref} from 'vue'
+
+import { useComics } from '@/composables/marvelApi'
+import type { Comic } from '@/types/marvel';
+import LoadingIndicator from './LoadingIndicator.vue'
+
+const isLoading: Ref<boolean> = ref(false);
+const data: Ref<Comic[] | undefined> = ref()
+
+const getComics = async () => {
+  isLoading.value = true;
+  const comics = await useComics();
+
+  data.value = comics.results;
+  isLoading.value = false;
+}
+
+onMounted(async () => {
+  await getComics()
+})
+</script>
+
+<style scoped>
+
+</style>
